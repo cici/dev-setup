11b81fac4a5459a25724aa9fca839d396e577bb7
Ci-Ci Thomson <cici.thomson@slalom.com>
2019-11-22 12:23:23 -0500

    Started install script and dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,38 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -Eueo pipefail
 
+# This is the main script to setup a new laptop
+# or add new configurations
+
+# Set env variables for the script
+DOTCONFIG=$HOME/.config
+DOTFILES=$HOME/.dotfiles
+
 # Include the log helpers and banner
 source ./log_utils.sh
 
+installFonts() {
+    log_info "Installing Fonts"
+
+    # Assume standard OSX fonts directory
+    fonts=~/Library/Fonts
+
+    # Copy fonts into place
+    find "$DOTFILES/fonts/" -name "*.[o,t]tf" -type f | while read -r file
+    do
+        cp -v "$file" "$fonts"
+    done
+}
+
+prepOS() {
+    log_info "Updating OSX and installing Xcode command line tools"
+    ./osxprep.sh
+}
+
+configureOS() {
+    log_info "Configuring OSX with preferred system defaults"
+    ./osx.sh
+}
 
 # Main Program
 print_main_banner
@@ -16,6 +45,10 @@ else
     for i in "$@"
     do
         case $i in
+            -pr--prep)
+                prepOS
+                shift
+                ;;
             -s|--sync)
                 doSync
                 doGitConfig
@@ -34,7 +67,7 @@ else
                 shift
                 ;;
             -c|--config)
-                doConfig
+                configureOS
                 shift
                 ;;
             -a|--all)
