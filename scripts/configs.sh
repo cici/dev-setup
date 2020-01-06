7301d428fd59d8309b067cfa2390ebbd9f9a2880
Ci-Ci Thomson <cici.thomson@slalom.com>
2020-01-06 11:48:19 -0500

    Refinements for gitconfig

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -8,6 +8,7 @@ set -Eueo pipefail
 # Set env variables for the script
 CONFIGFILES=$HOME/Documents/Projects/dev-setup/config_files
 
+cp -R $CONFIGFILES/.polar $HOME
 cp $CONFIGFILES/bat.conf $HOME
 cp $CONFIGFILES/ssh.config $HOME/.ssh/config
 cp $CONFIGFILES/youtube-dl.config $HOME
